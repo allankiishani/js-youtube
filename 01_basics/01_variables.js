1a4707dd7156c5e1668afb8292aebf9cf9c5ff30
allankiishani <147337237+allankiishani@users.noreply.github.com>
2024-02-23 09:57:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail ="ishani.allanki@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 // not allowed 
+
+accountEmail = "fshd@gj.com"
+accountPassword = "23344"
+accountCity = "bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope 
+
+use "let" instead of var 
+if you declare a variable in js but do not give it any value when you it print it , it will show you "undefined"
+*/
---
+++ 01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3 + 3) // we are using node js not browser 
+
+ let name = "hitesh"
+ let age = 18 
+ let isLoggedIn = false 
+
+ // number 
+ // bigint 
+ // string => ""
+ // boolean => true/false 
+ // null (standalone value and also js datatype)
+ // undefined => variable is declared but no value assigned 
+ // symbol (mainly used in react used to find uniqueness)
+ // the above are primitive datatypes 
+
+ console.log(typeof "ishani") // typeof is used to find type of variable or constant 
+
+// question asked in interview is below:
+
+console.log(typeof undefined); // undefined (output)
+console.log(typeof null); // object 
